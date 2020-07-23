f2ebd7e98f36a8a217206f68deb54cc51c812de8
Rick Belson <belsonrj@aol.com>
2020-07-23 00:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const names = ['Lisa', 'Kaitlin', 'Jan'];
- 
-function writeCards(names) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]} for the wonderful surprise gift!`);
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
- 
-  return names;
+  return thankYouCards
 }
-
 
  
  let countdown = 10;
